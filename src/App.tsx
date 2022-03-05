330957ed1ce5976209a28014fa772f294d481a5f
MarcoJul <82112168+MarcoJul@users.noreply.github.com>
2022-03-05 19:22:55 +0100

    style: added <Header/> and relative style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import classes from "./App.module.css";
 
+import Image from "./assets/home/background-home-desktop.jpg";
+
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <div>
+      <img src={Image} className={classes.background} />
+      <Header />
       <h1>Space</h1>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,31 @@
 import classes from "./Header.module.css";
 
+import { ReactComponent as Logo } from "../assets/shared/logo.svg";
+
 const Header = () => {
   return (
     <header>
-      <div className={classes.logo}></div>
+      <div className={classes.container}>
+        <div className={classes.logo}>
+          <Logo />
+        </div>
+        <nav className={classes.navigation}>
+          <ul className={classes.navigationIcons}>
+            <li>
+              <span className={classes.bold}>00</span> Home
+            </li>
+            <li>
+              <span className={classes.bold}>01</span> Destination
+            </li>
+            <li>
+              <span className={classes.bold}>02</span> Crew
+            </li>
+            <li>
+              <span className={classes.bold}>03</span> Technology
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
