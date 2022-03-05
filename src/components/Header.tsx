43edbb9f6b2f20ab406c6ab49b37ba68745cf7f7
MarcoJul <82112168+MarcoJul@users.noreply.github.com>
2022-03-05 18:34:40 +0100

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={classes.logo}></div>
+    </header>
+  );
+};
+
+export default Header;
